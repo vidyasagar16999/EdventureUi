4de78d999528414f62d84adab8689fcdd2531a43
vidyasagar16999 <vidyasagar16999@gmail.com>
2023-05-27 16:53:51 +0530

    c

--- src/app/edventure/homepage/homepage.component.ts
+++ src/app/edventure/homepage/homepage.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
-
+export class HomepageComponent implements OnInit, OnDestroy  {
+  private slideShowInterval: any;
   constructor() { }
 
-  ngOnInit(): void {
+  images: string[] = [
+    'assets/images/nagarjuna sagar.jpeg',
+    'assets/images/mount-opera-star-resort.jpg',
+    'assets/images/wonderla3.jfif'
+    // add paths to more images as needed
+  ];
+
+  currentImageIndex = 0;
+
+  ngOnInit() {
+    this.slideShowInterval = setInterval(() => {
+      this.nextImage();
+    }, 4000);
   }
 
+  ngOnDestroy() {
+    if (this.slideShowInterval) {
+      clearInterval(this.slideShowInterval);
+    }
+  }
+
+  previousImage() {
+    this.currentImageIndex--;
+    if (this.currentImageIndex < 0) {
+      this.currentImageIndex = this.images.length - 1;
+    }
+  }
+
+  nextImage() {
+    this.currentImageIndex++;
+    if (this.currentImageIndex >= this.images.length) {
+      this.currentImageIndex = 0;
+    }
+  }
 }
--- src/app/edventure/trips-services/trips-services.component.ts
+++ src/app/edventure/trips-services/trips-services.component.ts
@@ -20,8 +20,43 @@ export class TripsServicesComponent implements OnInit {
       activities: 'Activities for Trip 1.',
       pricing: 'Pricing details for Trip 1.',
       ageGroup: 'Age group recommendation for Trip 1.',
-      prerequisites: 'Prerequisite information for Trip 1.'
+      prerequisites: 'Prerequisite information for Trip 1.',
+      url: 'assets/images/nagarjuna sagar.jpeg',
+      alt: 'Image 1 description'
     },
+    {
+      title: 'Trip 2',
+      description: 'Detailed description of Trip 1.',
+      itinerary: 'Itinerary details for Trip 1.',
+      activities: 'Activities for Trip 1.',
+      pricing: 'Pricing details for Trip 1.',
+      ageGroup: 'Age group recommendation for Trip 1.',
+      prerequisites: 'Prerequisite information for Trip 1.',
+      url: 'assets/images/jala vihar.jpg',
+      alt: 'Image 2 description'
+    },
+    {
+      title: 'Trip 3',
+      description: 'Detailed description of Trip 1.',
+      itinerary: 'Itinerary details for Trip 1.',
+      activities: 'Activities for Trip 1.',
+      pricing: 'Pricing details for Trip 1.',
+      ageGroup: 'Age group recommendation for Trip 1.',
+      prerequisites: 'Prerequisite information for Trip 1.',
+      url: 'assets/images/mount-opera-star-resort.jpg',
+      alt: 'Image 3 description'
+    },
+    {
+      title: 'Trip 4',
+      description: 'Detailed description of Trip 1.',
+      itinerary: 'Itinerary details for Trip 1.',
+      activities: 'Activities for Trip 1.',
+      pricing: 'Pricing details for Trip 1.',
+      ageGroup: 'Age group recommendation for Trip 1.',
+      prerequisites: 'Prerequisite information for Trip 1.',
+      url: 'assets/images/ramoji film.jfif',
+      alt: 'Image 1 description'
+    }
     // Add more trip objects with their details here
   ];
 }
